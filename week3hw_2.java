23c4fb5a3b0e5e14be6bb788aebcddaf689cfbde
kimhyeyun <cqqwer@gmail.com>
2020-04-02 17:54:15 +0900

    week3 homework2 - switch문을 이용해 * 개수입력받고 갯수만큼 *출력

---
+++ week3hw_2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class week3hw_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		
+		switch(num){
+		case 1:
+			System.out.println("*");
+			break;
+		case 2:
+			System.out.println("**");
+			break;
+		case 3:
+			System.out.println("***");
+			break;
+		case 4:
+			System.out.println("****");
+			break;
+		case 5:
+			System.out.println("*****");
+			break;
+		case 6:
+			System.out.println("******");
+			break;
+		case 7:
+			System.out.println("*******");
+			break;
+		case 8:
+			System.out.println("********");
+			break;
+		case 9:
+			System.out.println("*********");
+			break;
+		case 10:
+			System.out.println("**********");
+			break;
+		}
+		
+	}
+
+}
